6d2812cb67cab27da52139e830196cba532f354d
Brown Nguyễn <brownnguyen@gmail.com>
2020-11-13 16:22:13 +0700

    add__full

---
+++ js/general.js
@@ -0,0 +1,19 @@
+var menuBurger = document.querySelector('.menu-burger');
+var headerList = document.querySelector('.header-list');
+var layer = document.querySelector('.drawer .layer');
+menuBurger.onclick = function () {
+    menuBurger.classList.toggle('active');
+    if (menuBurger.classList.contains('active')) {
+        headerList.classList.add('active');
+        layer.classList.add('active')
+    }
+    else {
+        headerList.classList.remove('active');
+        layer.classList.remove('active');
+    }
+}
+layer.onclick = function () {
+    menuBurger.classList.remove('active');
+    headerList.classList.remove('active');
+    layer.classList.remove('active')
+}
